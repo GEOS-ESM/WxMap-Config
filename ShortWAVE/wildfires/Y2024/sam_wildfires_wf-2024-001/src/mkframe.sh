eea7c7554e207303b11ff74e7d1bbcee724447ac
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2024-10-10 09:43:31 -0400

    New case for SAM wildfires.

---
+++ ShortWAVE/wildfires/Y2024/sam_wildfires_wf-2024-001/src/mkframe.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+app=`realpath $0`
+app_path=`dirname $app`
+config_path=`dirname $app_path`
+catalog_id=`basename $config_path`
+year=`echo $catalog_id | cut -d'-' -f2`
+
+in_dir=$NOBACKUP/ShortWAVE/wildfires/Y${year}/$catalog_id/images
+out_dir=$NOBACKUP/ShortWAVE/wildfires/Y${year}/$catalog_id/frames
+
+mkdir -p $out_dir
+
+cd $out_dir
+cp -p $app_path/*.py .
+cp -p $app_path/*cbar* .
+mkframe.py $in_dir/*.png
+
+/bin/rm $out_dir/*.py
+/bin/rm $out_dir/*.pyc
+/bin/rm $out_dir/*cbar*
+
+echo $out_dir
+
+exit 0
