cbf9b94400a4a8c2ba61a7b5b511351a1baf479e
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2026-01-23 18:17:50 -0500

    Adding brown carbon visualization.

---
+++ ShortWAVE/air_quality/Y2025/brown_carbon_aq-2025-001/src/plot.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [sccyymmdd] [eccyymmdd]" 2>&1
+  exit 1
+fi
+
+sdate=$1
+edate=$2
+
+app=`realpath $0`
+app_path=`dirname $app`
+config_path=`dirname $app_path`
+catalog_id=`basename $config_path`
+year=`echo $catalog_id | cut -d'-' -f2`
+
+wxmap.py --config $config_path \
+         --stream CONUS2KM \
+         --start_dt $sdate \
+         --end_dt $edate \
+         --t_deltat PT30M \
+         --field aod \
+         --region global \
+         --fullframe --lights_off --no_title --no_label --no_logo \
+         --geometry 4320x2160 \
+         --oname $NOBACKUP/ShortWAVE/air_quality/Y${year}/$catalog_id/images/$catalog_id.nasa.gmao.conus2km.analysis.aod.image.4320x2160.%Y%m%d%H%M.png
