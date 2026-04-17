99b2b3c92e06de36f69eea745f7c750d0c8792df
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2026-04-17 11:31:30 -0400

    Adding BCCMASS codes for 2023 smoke events over U.S. and Canada.

---
+++ ShortWAVE/wildfires/Y2023/canada_wildfires_wf-2023-008/src/plot.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [sccyymmdd] [eccyymmdd]" 2>&1
+  exit 1
+fi
+
+sdate=$1
+edate=$2
+
+app=`realpath $0`
+app_path=`dirname $app`
+config_path=`dirname $app_path`
+catalog_id=`basename $config_path`
+year=`echo $catalog_id | cut -d'-' -f2`
+
+wxmap.py --config $config_path \
+         --stream GEOSFPANA \
+         --start_dt $sdate \
+         --end_dt $edate \
+         --t_deltat 1 \
+         --field bccmass \
+         --region global \
+         --fullframe --lights_off --no_title --no_label --no_logo \
+         --geometry 7680x3840 \
+         --save_cbar \
+         --oname $NOBACKUP/ShortWAVE/wildfires/Y${year}/$catalog_id/images/$catalog_id.nasa.gmao.geos-fp.analysis.bccmass.image.7680x3840.%Y%m%d%H.png
