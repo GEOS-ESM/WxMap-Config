60c10ce1ed2f0a9a32312d0eb75219bcbc6ce38c
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2026-07-29 14:00:51 -0400

    Adding event for fires over Europe.

---
+++ ShortWAVE/wildfires/Y2026/eu_wildfires_wf-2026-001/src/plot.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [sccyymmdd] [eccyymmdd]" 2>&1
+  exit 1
+fi
+
+sdate=$1
+edate=$2
+
+app=`realpath $0`
+app_path=`dirname $app`
+config_path=`dirname $app_path`
+catalog_id=`basename $config_path`
+year=`echo $catalog_id | cut -d'-' -f2`
+
+echo $config_path
+
+wxmap.py --config $config_path \
+         --stream CONUS2KM \
+         --start_dt $sdate \
+         --end_dt $edate \
+         --t_deltat 1 \
+         --field smoke \
+         --region global \
+         --fullframe --lights_off --no_title --no_label --no_logo \
+         --geometry 7680x3840 \
+         --oname $NOBACKUP/ShortWAVE/wildfires/Y${year}/$catalog_id/images/$catalog_id.nasa.gmao.geos-cam.analysis.smoke.image.7680x3840.%Y%m%d%H.png
+
+
+# 7680x3840
+# 4096x2048
