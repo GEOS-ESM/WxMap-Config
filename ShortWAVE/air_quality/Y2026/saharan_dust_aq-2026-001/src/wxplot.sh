72fcbc5e43e4fa21af711df412e96cac96bbb0d3
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2026-08-03 11:08:11 -0400

    Adding instance for Saharan dust event.

---
+++ ShortWAVE/air_quality/Y2026/saharan_dust_aq-2026-001/src/wxplot.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [sccyymmdd] [eccyymmdd]" 2>&1
+  exit 1
+fi
+
+sdate=$1
+edate=$2
+
+app=`realpath $0`
+app_path=`dirname $app`
+config_path=`dirname $app_path`
+catalog_id=`basename $config_path`
+year=`echo $catalog_id | cut -d'-' -f2`
+
+cat >tt <<EOF
+shortwave:
+
+  configs: [$config_path]
+  stream: GEOSANA
+  start_dt: P0D
+  end_dt: P30DT23H
+  t_deltat: PT1H
+  options: --fullframe --lights_off --no_title --no_label --no_logo --geometry 3840x2160
+  oname: $NOBACKUP/ShortWAVE/air_quality/Y${year}/$catalog_id/images/$catalog_id.nasa.gmao.geos-fp.analysis.aod.image.2160p.%Y%m%d%H.png
+  levels: [0]
+  fields: [aod]
+EOF
